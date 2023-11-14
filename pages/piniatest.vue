7fa33f6640763240b8b534e6a2c0cfcb23484e1d
joel <joel@cntt.co.kr>
2023-11-14 15:22:35 +0900

    pinia 연동 및 테슽 코드 추가

---
+++ pages/piniatest.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <NuxtLink to="/piniatest2">piniatest2</NuxtLink>
+  </nav>
+  <div>
+    <input type="button" @click="store.increment()" value="increment" />
+    {{ store.count }}
+    {{ store.doubleCount }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useCounterStore } from "../stores/counter";
+
+const store = useCounterStore();
+</script>
+
+<style scoped></style>
---
+++ pages/piniatest2.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <NuxtLink to="/piniatest">piniatest</NuxtLink>
+  </nav>
+  <div>
+    <input type="button" @click="store.increment()" value="increment" />
+    {{ store.count }}
+    {{ store.doubleCount }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useCounterStore } from "../stores/counter";
+
+const store = useCounterStore();
+</script>
+
+<style scoped></style>
